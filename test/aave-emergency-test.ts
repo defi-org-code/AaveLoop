27a24f1f0c164b93ca63bbb50bad9add7106b552
DanielZlotin <zlotindaniel@gmail.com>
2021-05-06 22:45:31 +0300

    delegate call cool test

--- test/aave-emergency-test.ts
+++ test/aave-emergency-test.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import { aaveloop, expectOutOfPosition, initOwnerAndUSDC, owner, POSITION } from "./test-base";
 import { bn6, max } from "../src/utils";
 import { USDC } from "../src/token";
+import { contract } from "../src/extensions";
 
 describe("AaveLoop Emergency Tests", () => {
   beforeEach(async () => {
@@ -28,7 +29,7 @@ describe("AaveLoop Emergency Tests", () => {
   it("emergency function call", async () => {
     await USDC().methods.transfer(aaveloop.options.address, POSITION).send({ from: owner });
 
-    const encoded = await USDC().methods.transfer(owner, POSITION).encodeABI();
+    const encoded = USDC().methods.transfer(owner, POSITION).encodeABI();
     await aaveloop.methods.emergencyFunctionCall(USDC().options.address, encoded).send({ from: owner });
 
     expect(await USDC().methods.balanceOf(aaveloop.options.address).call()).bignumber.zero;
@@ -38,10 +39,23 @@ describe("AaveLoop Emergency Tests", () => {
   it("emergency function delegate call", async () => {
     await USDC().methods.transfer(aaveloop.options.address, POSITION).send({ from: owner });
 
-    const encoded = await aaveloop.methods.renounceOwnership().encodeABI();
-    await aaveloop.methods.emergencyFunctionDelegateCall(aaveloop.options.address, encoded).send({ from: owner });
+    const compoundLoopAddress = "0x8bd210Fff94C41640F1Fd3E6A6063d04e2f10eEb";
+    const compoundLoopABI = [
+      {
+        inputs: [],
+        name: "safeTransferUSDCToOwner",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+    ];
+    const compoundLoop = contract(compoundLoopABI, compoundLoopAddress);
 
-    expect(await aaveloop.methods.owner().call()).eq("0x0000000000000000000000000000000000000000");
+    const encoded = await compoundLoop.methods["safeTransferUSDCToOwner"]().encodeABI();
+    await aaveloop.methods.emergencyFunctionDelegateCall(compoundLoopAddress, encoded).send({ from: owner });
+
+    expect(await USDC().methods.balanceOf(aaveloop.options.address).call()).bignumber.zero;
+    expect(await USDC().methods.balanceOf(owner).call()).bignumber.eq(POSITION);
   });
 
   it("exit position one by one manually", async () => {
